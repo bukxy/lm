413d63926ba5514cdfb4a68fd4d6e00492d8a343
bukxy <peron.nicolas2@gmail.com>
2019-12-29 18:11:14 +0100

    Add Migrations

---
+++ src/Migrations/Version20191227195648.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191227195648 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE boost_territory (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, boost_territory_category_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, category VARCHAR(255) NOT NULL, INDEX IDX_94EC4159A76ED395 (user_id), INDEX IDX_94EC415940487763 (boost_territory_category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE boost_territory_category (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, INDEX IDX_51BDF1C9A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE boost_territory ADD CONSTRAINT FK_94EC4159A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE boost_territory ADD CONSTRAINT FK_94EC415940487763 FOREIGN KEY (boost_territory_category_id) REFERENCES boost_territory_category (id)');
+        $this->addSql('ALTER TABLE boost_territory_category ADD CONSTRAINT FK_51BDF1C9A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE boost_territory DROP FOREIGN KEY FK_94EC415940487763');
+        $this->addSql('DROP TABLE boost_territory');
+        $this->addSql('DROP TABLE boost_territory_category');
+    }
+}
