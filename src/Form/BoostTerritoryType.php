c811e64c596e5c6c43aab1f085cd134bd22627a6
bukxy <peron.nicolas2@gmail.com>
2019-12-30 19:09:15 +0100

    Set user id directly by php

--- src/Form/BoostTerritoryType.php
+++ src/Form/BoostTerritoryType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\BoostTerritory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class BoostTerritoryType extends AbstractType
 {
