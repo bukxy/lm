b0c2dff4c89e85a95d6505ef4dac1bba2060a717
bukxy <peron.nicolas2@gmail.com>
2020-01-17 15:48:24 +0100

    Add TinyMce

--- assets/js/back/backcss.js
+++ assets/js/back/backcss.js
@@ -1,2 +1,7 @@
 import('../../css/back/global.css');
 import('../../css/back/sb-admin-2.css');
+
+tinymce.init({
+    selector: 'textarea',  // change this value according to your HTML
+    height: '650'
+});
