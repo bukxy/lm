69fcf60ce1ff4ed75126b93c410330886e548d8a
bukxy <peron.nicolas2@gmail.com>
2020-01-17 15:50:27 +0100

    Add sticky leftmenu

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,1 +1,21 @@
 import('../css/front/front.css');
+import('../css/front/familiar.css');
+import('../css/front/construction.css');
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(document).ready(function () {
+    var stickyTop = $('.leftNav .navbar').offset().top;
+
+    $(window).scroll(function () {
+        var windowTop = $(window).scrollTop();
+        if (stickyTop < windowTop && $(".leftNav").height() > windowTop) {
+            $('.leftNav .navbar').addClass('sticky');
+        } else {
+            $('.leftNav .navbar').removeClass('sticky');
+            $('.leftNav .navbar').css('transition','.5s');
+        }
+    });
+});
