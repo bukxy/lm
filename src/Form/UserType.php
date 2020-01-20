c5956beb0388caf71533943e10b41386835fc076
bukxy <peron.nicolas2@gmail.com>
2020-01-20 14:13:55 +0100

    Add repeat password

---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('pseudo')
+            ->add('password', RepeatedType::class, [
+                'type'      => PasswordType::class,
+                'invalid_message'   => 'Les deux mots de passe doivent correspondre',
+                'options'   => ['attr' => ['class' => 'password-field']],
+                'required'  => false,
+                'mapped'    => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
