107df83c85cd7926570a18041e2cee17f3eb6305
bukxy <peron.nicolas2@gmail.com>
2020-01-21 14:30:56 +0100

    create an adminType for add a select input for choose roles

---
+++ src/Form/AdminUserType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class AdminUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('pseudo')
+            ->add('password', RepeatedType::class, [
+                'type'      => PasswordType::class,
+                'invalid_message'   => 'Les deux mots de passe doivent correspondre',
+                'options'   => ['attr' => ['class' => 'password-field']],
+                'required'  => false,
+                'mapped'    => false
+            ])
+            ->add('roles', ChoiceType::class, [
+                'required'  => false,
+                'multiple'  => true,
+                'expanded'  => true, // render check-boxes
+                'attr'      => ['class' => 'form-group'],
+                'choices'   => [
+                    'Admin' => 'ROLE_SUPER_ADMIN',
+                    'Accés au panel admin'      => 'ROLE_ADMIN_ACCESS',
+                    'Gestion des utilisateurs'  => 'ROLE_ADMIN_USERS',
+                    'Gestion des images'        => 'ROLE_ADMIN_IMAGES',
+                    'Gestion des boosts'        => 'ROLE_ADMIN_BOOSTS',
+                    'Gestion des constructions' => 'ROLE_ADMIN_CONSTRUCTIONS',
+                    'Gestion des recherches'    => 'ROLE_ADMIN_RESEARCHS',
+                    'Gestion des familiers'     => 'ROLE_ADMIN_FAMILIARS',
+                    'Gestion des evenements'    => 'ROLE_ADMIN_EVENTS',
+                    // ...
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
