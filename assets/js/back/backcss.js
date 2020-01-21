ae91cd4d4f5cc58b384c6d71d89d5268a7c93b1c
bukxy <peron.nicolas2@gmail.com>
2020-01-21 14:27:54 +0100

    Add class for perms form

--- assets/js/back/backcss.js
+++ assets/js/back/backcss.js
@@ -5,3 +5,15 @@ tinymce.init({
     selector: 'textarea',  // change this value according to your HTML
     height: '650'
 });
+
+$(document).ready(function () { // css user Permissions
+    let div = $('.user-perms > div div');
+    div.addClass('form-group custom-control custom-switch');
+    div.removeClass('form-check');
+
+    let input = $('.user-perms > div div input');
+    input.addClass('custom-control-input')
+
+    let label = $('.user-perms > div div label');
+    label.addClass('custom-control-label')
+});
