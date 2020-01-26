609bfaef5358a67dc911a9bd41f90aa5f9782555
bukxy <peron.nicolas2@gmail.com>
2020-01-26 15:56:27 +0100

    Add user Rank

---
+++ src/Repository/UserRankRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\UserRank;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method UserRank|null find($id, $lockMode = null, $lockVersion = null)
+ * @method UserRank|null findOneBy(array $criteria, array $orderBy = null)
+ * @method UserRank[]    findAll()
+ * @method UserRank[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRankRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserRank::class);
+    }
+
+    // /**
+    //  * @return UserRank[] Returns an array of UserRank objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?UserRank
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
