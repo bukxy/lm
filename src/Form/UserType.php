f035aa7d664698081954f4531d97b0c9fd8c4f02
bukxy <peron.nicolas2@gmail.com>
2020-02-12 14:26:52 +0100

    Add contraints

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -17,11 +18,21 @@ class UserType extends AbstractType
             ->add('email')
             ->add('pseudo')
             ->add('password', RepeatedType::class, [
-                'type'      => PasswordType::class,
-                'invalid_message'   => 'Les deux mots de passe doivent correspondre',
-                'options'   => ['attr' => ['class' => 'password-field']],
-                'required'  => false,
-                'mapped'    => false
+                'type' => PasswordType::class,
+                'mapped'    => false,
+                'required' => false,
+                'invalid_message' => 'Les deux mots de passe doivent correspondre',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit faire au minimum {{ limit }} characters',
+                        'max' => 255,
+                        'maxMessage' => 'Votre mot de passe doit faire au maximum {{ limit }} characters',
+                    ]),
+                ],
             ])
         ;
     }
