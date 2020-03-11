1681c6162c2c5dc43be936e0054e246e3626034f
bukxy <peron.nicolas2@gmail.com>
2020-03-11 16:21:23 +0100

    edit for the news

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\NewsRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FrontController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(NewsRepository $n, Request $request, PaginatorInterface $paginator)
     {
-        return $this->render('front/index.html.twig');
+        $news = $n->findAll();
+        $result = $paginator->paginate(
+            $news,
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            1 // Nombre de résultats par page
+        );
+
+        return $this->render('front/news.html.twig', [
+            'news' => $result
+        ]);
     }
 }
