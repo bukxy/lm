7986cb4683174bdbf4732e38217364c6229e4aca
bukxy <peron.nicolas2@gmail.com>
2020-03-11 16:14:22 +0100

    get one or by category

---
+++ src/Controller/BoostTerritoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BoostTerritory;
+use App\Entity\BoostTerritoryCat;
+use App\Repository\ImageRepository;
+use App\Repository\BoostTerritoryRepository;
+use App\Repository\BoostTerritoryCatRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+* @Route("/boost", name="boost_")
+*/
+class BoostTerritoryController extends AbstractController
+{
+    /**
+    * @Route("/", name="list")
+    */
+    public function index(BoostTerritoryRepository $b, BoostTerritoryCatRepository $bc, Request $request)
+    {                
+        return $this->render('front/boost.html.twig', [
+            'boostIcon' => $b->findAll(),
+            'boostCat' => $bc->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="one", methods={"POST"})
+     */
+    public function oneFamiliar(BoostTerritory $b = null, Request $request)
+    {
+        if ($b) {
+            $url = $request->server->get('HTTP_HOST');
+
+            return $this->json([
+                    'message'       =>  true,
+                    'url'           =>  'https://'. $url . '/uploads',
+                    'defaultImage'  =>  'https://'. $url . '/uploads/default.png',
+                    'result'        =>  $b
+                ], 200, [],
+                    ['groups' => ['BoostTerritoryByCat:read', 'image:read']]
+            );
+        } else {
+            return $this->json([
+                'message' => false
+            ], 200);
+        }
+    }
+
+    /**
+     * @Route("/category/{id}", name="listByCategory", methods={"POST"})
+     */
+    public function listByCategory(BoostTerritoryCat $bcat = null, BoostTerritoryRepository $bRepo, Request $request)
+    {
+        if ($bcat) {
+            $url = $request->server->get('HTTP_HOST');
+
+            return $this->json([
+                    'message'       =>  true,
+                    'url'           =>  'https://'. $url . '/uploads/images/',
+                    'defaultImage'  =>  'https://'. $url . '/uploads/familiar.png',
+                    'result'        =>  $bRepo->findBy(['category' => $bcat->getId()])
+                ], 200, [],
+                    ['groups' => ['BoostTerritoryByCat:read', 'image:read']]
+            );
+        } else {
+            return $this->json([
+                'message' => false
+            ], 200);
+        }
+    }
+}
